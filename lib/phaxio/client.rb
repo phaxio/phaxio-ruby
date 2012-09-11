157bcbe68b929f3a181df31b50caaba3e17ef57f
Brett Chalupa <brettchalupa@gmail.com>
2012-09-10 23:06:47 -0400

    changing the way path worked and refactor the Client class

--- lib/phaxio/client.rb
+++ lib/phaxio/client.rb
@@ -6,13 +6,9 @@ module Phaxio
     attr_accessor :api_key, :api_secret, :faxes_sent_this_month,
                   :faxes_sent_today, :balance
 
-    def get_account_status
-      status = get(path + "/accountStatus", options[:query].merge(api_key: api_key,
-                                                         api_secret:api_secret))
-
-      faxes_sent_this_month = status.faxes_sent_this_month
-      faxes_sent_today = status.faxes_sent_today
-      balance = status.balance
+    def initialize(api_key, api_secret)
+      self.api_key = api_key
+      self.api_secret = api_secret
     end
 
     def create_fax(options)
@@ -29,21 +25,55 @@ module Phaxio
       end
     end
 
-    def check_fax_status(fax_id)
-      if !fax_id 
-        raise StandardError, "You must include a fax id"
+    def send_fax(path, fax)
+      if path.nil?
+        @path = "send"
+      else
+        @path = path
       end
 
-      get(path, {id:fax_id})
-    end
-
-    def send_fax(path, fax)
-      post(path, options[:query].merge(to: fax.to_number, filename: fax.filename,
+      post(@path, options[:query].merge(to: fax.to_number, filename: fax.filename,
                                        string_data: fax.string_data, string_data_type: fax.string_data_type,
                                        batch: fax.batch, batch_delay: fax.batch_delay,
                                        batch_collision_avoidance: fax.batch_collision_avoidance,
                                        callback_url: fax.callback_url, cancel_timeout: fax.cancel_timeout,
                                        api_key: api_key, api_secret: api_secret))
+    end
+
+    def cancel_fax(path, options)
+      if path.nil?
+        @path = "/faxCancel"
+      else
+        @path = path
+      end
+    end
+
+    def check_fax_status(path, fax_id)
+      if path.nil?
+        @path = "/faxStatus"
+      else
+        @path = path
+      end
+
+      if !fax_id 
+        raise StandardError, "You must include a fax id"
+      end
+
+      get(@path, {id:fax_id})
+    end
+
+
+    def get_account_status(path)
+      if path.nil?
+        @path = "/accountStatus"
+      else
+        @path = path
+      end
+
+      status = get(@path, options[:query].merge(api_key: api_key, api_secret:api_secret))
+      faxes_sent_this_month = status.faxes_sent_this_month
+      faxes_sent_today = status.faxes_sent_today
+      balance = status.balance
     end
   end
 end
