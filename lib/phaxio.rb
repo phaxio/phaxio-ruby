a1c4c9d873443a96a52c0060eff74282f8e94461
Brett Chalupa <brettchalupa@gmail.com>
2012-09-12 12:00:51 -0400

    working on creating tests

--- lib/phaxio.rb
+++ lib/phaxio.rb
@@ -1,12 +1,5 @@
+require "httparty"
+
 require "phaxio/version"
 require "phaxio/config"
 require "phaxio/client"
-
-module Phaxio
-  def config
-    Config.configure do |client|
-      client.api_key = "abc123"
-      client.api_secret = "abc123"
-    end
-  end
-end
--- test/test_client.rb
+++ test/test_client.rb
@@ -1,5 +1,7 @@
 require "test_helper"
 
 class TestClient < Test::Unit::TestCase
-
+  def setup
+    @client = Phaxio::Client.new("abc123", "def456")
+  end
 end
