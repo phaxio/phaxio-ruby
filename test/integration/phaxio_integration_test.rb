37ed5b0030c7c11d3f5489cb54dab817964f39ad
Julien Negrotto <jtnegrotto@gmail.com>
2016-02-10 13:49:01 -0600

    Revise integration tests

--- test/integration/phaxio_integration_test.rb
+++ test/integration/phaxio_integration_test.rb
@@ -4,36 +4,42 @@ require 'json'
 class PhaxioIntegrationTest < MiniTest::Test
   def setup
     FakeWeb.clean_registry
-
-    Phaxio.config do |config|
-      config.api_key = ENV.fetch('PHAXIO_TEST_API_KEY')
-      config.api_secret = ENV.fetch('PHAXIO_TEST_API_SECRET')
-    end
+    configure
 
     @fax_number = '2025550168'
-    @responses_path = File.expand_path('../../support/responses/', __FILE__)
   end
 
   def test_resend_fax
-    original_response = Phaxio.send_fax(
-      to: @fax_number, string_data: 'test', test_fail: 'faxError'
-    )
-    original_fax_id = original_response['faxId']
-    sleep(5)
-    response = Phaxio.resend_fax(id: original_fax_id).to_h
-    new_fax_id = response['data']['faxId']
-    expected_string = File.read("#{@responses_path}/resend_success.json")
-                          .gsub('FAX_ID', new_fax_id.to_s)
-    expected = JSON.parse expected_string
-    assert_equal expected, response
+    send_fax(test_fail: 'faxError')
+    response = Phaxio.resend_fax(id: @send_fax_response['faxId'])
+    assert response['success']
+    assert_equal 'Fax queued for resend', response['message']
+    assert response['data']['faxId'].integer?
   end
 
   def test_delete_fax
-    original_response = Phaxio.send_fax(to: @fax_number, string_data: 'test')
-    id = original_response['faxId']
+    send_fax
+    response = Phaxio.delete_fax(id: @send_fax_response['faxId'])
+    assert response['success']
+    assert_equal 'Deleted fax successfully!', response['message']
+  end
+
+  private
+
+  def configure
+    Phaxio.config do |config|
+      config.api_key = ENV.fetch 'PHAXIO_TEST_API_KEY'
+      config.api_secret = ENV.fetch 'PHAXIO_TEST_API_SECRET'
+    end
+  rescue KeyError
+    raise(KeyError, 'You must set PHAXIO_TEST_API_KEY and ' \
+      'PHAXIO_TEST_API_SECRET to run integration tests')
+  end
+
+  # Convenience method for actions requiring an existing fax
+  def send_fax(options = {})
+    params = options.merge(to: @fax_number, string_data: 'test')
+    @send_fax_response = Phaxio.send_fax(params)
     sleep(5)
-    expected = JSON.parse File.read("#{@responses_path}/delete_success.json")
-    response = Phaxio.delete_fax(id: id).to_h
-    assert_equal expected, response
   end
 end
