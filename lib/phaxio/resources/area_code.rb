53e41cc68c6c448dd615569417e9fc4e3800f658
Julien Negrotto <jtnegrotto@gmail.com>
2017-11-06 12:16:17 -0600

    Flesh out area code records

--- lib/phaxio/resources/area_code.rb
+++ lib/phaxio/resources/area_code.rb
@@ -4,6 +4,13 @@ module Phaxio
     #
     # @see Phaxio::Resources::PhoneNumber.list_available_area_codes
     class AreaCode < Resource
+      # @!attribute country_code
+      # @!attribute area_code
+      # @!attribute city
+      # @!attribute state
+      # @!attribute country
+      # @!attribute toll_free
+      has_normal_attributes %w[country_code area_code city state country toll_free]
     end
   end
 end
