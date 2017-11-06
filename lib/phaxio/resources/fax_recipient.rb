04366b640e0350707ec237984c3c7c51241b575a
Julien Negrotto <jtnegrotto@gmail.com>
2017-11-06 12:23:40 -0600

    Flesh out fax recipient records

--- lib/phaxio/resources/fax_recipient.rb
+++ lib/phaxio/resources/fax_recipient.rb
@@ -1,6 +1,20 @@
 module Phaxio
   module Resources
     class FaxRecipient < Resource
+      # @!attribute phone_number
+      # @!attribute status
+      # @!attribute retry_count
+      # @!attribute bitrate
+      # @!attribute resolution
+      # @!attribute error_type
+      # @!attribute error_message
+      # @!attribute error_id
+      has_normal_attributes %w[
+        phone_number status retry_count bitrate resolution error_type error_message error_id
+      ]
+
+      # @!attribute completed_at
+      has_time_attributes %w[completed_at]
     end
   end
 end
