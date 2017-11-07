17d6f3e7d03f2fb28832f9f2b4136da1cc669508
Julien Negrotto <jtnegrotto@gmail.com>
2017-11-07 14:03:52 -0600

    Move MimeTypeHelper into Phaxio namespace

--- lib/phaxio/helpers/mime_type_helper.rb
+++ lib/phaxio/helpers/mime_type_helper.rb
@@ -1,11 +1,14 @@
-module MimeTypeHelper
-  class << self
-    def extension_for_mimetype mimetype
-      MIME::Types[mimetype].first.extensions.first
-    end
+module Phaxio
+  # @api private
+  module MimeTypeHelper
+    class << self
+      def extension_for_mimetype mimetype
+        MIME::Types[mimetype].first.extensions.first
+      end
 
-    def mimetype_for_file file_path
-      MIME::Types.of(file_path).first.content_type
+      def mimetype_for_file file_path
+        MIME::Types.of(file_path).first.content_type
+      end
     end
   end
 end
--- spec/helpers/mime_type_helper_spec.rb
+++ spec/helpers/mime_type_helper_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-describe MimeTypeHelper do
+describe Phaxio::MimeTypeHelper do
   it 'gets the mimetype for a filename' do
-    expect(MimeTypeHelper.mimetype_for_file('test.pdf')).to eq('application/pdf')
+    expect(subject.mimetype_for_file('test.pdf')).to eq('application/pdf')
   end
 
   it 'gets the extension for a mimetype' do
-    expect(MimeTypeHelper.extension_for_mimetype('application/pdf')).to eq('pdf')
+    expect(subject.extension_for_mimetype('application/pdf')).to eq('pdf')
   end
 end
