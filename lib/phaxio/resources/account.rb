bfc6a7ed990fe6145c308a2e7e1603afc2cb48e8
Julien Negrotto <jtnegrotto@gmail.com>
2017-11-07 09:58:22 -0600

    Document Accounts

--- lib/phaxio/resources/account.rb
+++ lib/phaxio/resources/account.rb
@@ -5,15 +5,23 @@ module Phaxio
       ACCOUNT_PATH = 'account'
       private_constant :ACCOUNT_PATH
 
+      # @return [Integer] Your current account funds balance in cents.
       # @!attribute balance
+
+      # @return [Hash<String: Integer>] A hash of the number of faxes sent and received today.
       # @!attribute faxes_today
+
+      # @return [Hash<String: Integer>] A hash of the number of faxes sent and received this month.
       # @!attribute faxes_this_month
       has_normal_attributes %w[balance faxes_today faxes_this_month]
 
       class << self
         # Get information about your Phaxio account, including your balance, number of faxes sent
         # today, and number of faxes sent this week.
-        #
+        # @param params [Hash]
+        #   Any parameters to send to Phaxio. This action does not have any unique parameters.
+        # @return [Phaxio::Resources::Acount] Your account information.
+        # @raise [Phaxio::Error::PhaxioError]
         # @see https://www.phaxio.com/docs/api/v2/account/status
         def get params = {}
           response = Client.request :get, account_status_endpoint, params
